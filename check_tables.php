ade98a6f2277ff58f75242055b2734bee84e9b15
mohamednassimkr <168354288+mohamednassimkr@users.noreply.github.com>
2026-04-06 21:53:33 +0100

    Revert "khdmt nassim"

--- check_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=agrimans', 'root', '');
-$result = $pdo->query('SHOW TABLES');
-echo "Tables in agrimans database:\n";
-while ($row = $result->fetch(PDO::FETCH_NUM)) {
-    echo "  - " . $row[0] . "\n";
-}
-?>
