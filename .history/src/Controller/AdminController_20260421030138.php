880ebd3ea257be39e5a571ede1adb133a1272e46
Mohamed <mohamed.daoudi@esprit.tn>
2026-05-05 14:34:03 +0100

    new version

--- .history/src/Controller/AdminController_20260421030138.php
+++ .history/src/Controller/AdminController_20260421030138.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Request;
-Doctrine\ORM\EntityManagerInterface
+Doctrine\ORM\EntityManagerInterface;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin_dashboard')]
